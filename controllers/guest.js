cd418e849a754dc541059d79b4832373e2ac1199
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-21 01:07:21 +0700

    Add apiLoginGuest

--- controllers/guest.js
+++ controllers/guest.js
@@ -18,4 +18,18 @@ export default class GuestController {
             res.status(500).json({ status: false, error: e.message })
         }
     }
+
+    static async apiLoginGuest(req, res, next) {
+        try {
+            const email = req.body.email
+            const password = req.body.password
+
+            GuestModel.getGuestByLogin({ email, password }, (err, data) => {
+                if (err) res.status(500).json({ status: false, error: err })
+                else res.send({ status: true, data });
+            })
+        } catch (e) {
+            res.status(500).json({ status: false, error: e.message })
+        }
+    }
 }
