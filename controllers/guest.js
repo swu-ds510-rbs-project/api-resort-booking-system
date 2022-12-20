20075d2512707c9ba2beab715e6a70edc3db217a
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-21 00:22:39 +0700

    Add apiRegisterGuest

---
+++ controllers/guest.js
@@ -0,0 +1,21 @@
+import GuestModel from "../models/guest.js"
+
+export default class GuestController {
+
+    static async apiRegisterGuest(req, res, next) {
+        try {
+            const firstname = req.body.firstname
+            const lastname = req.body.lastname
+            const tel = req.body.tel
+            const email = req.body.email
+            const password = req.body.password
+
+            GuestModel.addGuest({ firstname, lastname, tel, email, password }, (err, data) => {
+                if (err) res.status(500).json({ status: false, error: err })
+                else res.send({ status: true, data });
+            })
+        } catch (e) {
+            res.status(500).json({ status: false, error: e.message })
+        }
+    }
+}
