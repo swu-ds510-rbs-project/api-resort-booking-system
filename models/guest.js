1d1ef203b9520c455fb905540d97827fadfc90e9
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-21 01:06:56 +0700

    Add getGuestByLogin

--- models/guest.js
+++ models/guest.js
@@ -1,6 +1,9 @@
 import db from '../database/connection.js';
 import bcrypt from "bcrypt";
 const saltRounds = 10;
+import jwt from "jsonwebtoken";
+import config from 'config';
+const SECRET = config.get("SECRET")
 
 export default class Guest {
 
@@ -23,6 +26,30 @@ export default class Guest {
             })
         } catch (e) {
             console.error(`Unable to register: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async getGuestByLogin(filter, result) {
+        try {
+            let query = "SELECT * FROM guest where email = ?"
+            db.query(query, filter.email, (err, res) => {
+                if (err) result(err, null);
+                else if (res.length === 0) result("not found email", null);
+                else {
+                    bcrypt.compare(filter.password, res[0].password, (err, response) => {
+                        if (!response) result("password invaild", null);
+                        else {
+                            var token = jwt.sign({ email: filter.email }, SECRET, { expiresIn: '10h' });
+                            res[0].token = token
+                            delete res[0].password;
+                            result(null, res);
+                        }
+                    });
+                }
+            })
+        } catch (e) {
+            console.error(`Unable to login: ${e}`)
             return { error: e }
         }
     }
