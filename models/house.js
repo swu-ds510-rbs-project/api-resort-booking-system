ff72ba1dccc178367afcbf0b8c64731c79fe82b4
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-20 19:18:08 +0700

    Update using placeholder in sql query

--- models/house.js
+++ models/house.js
@@ -3,10 +3,11 @@ import db from '../database/connection.js';
 export default class House {
     static async getAvailableHouses(filter, result) {
         const query = `select * from house where house_id not in (
-            select house_id from booking
-            where (checkin_date >= "${filter.checkin_date}" and checkin_date < "${filter.checkout_date}")
-            or (checkout_date > "${filter.checkin_date}" and checkout_date < "${filter.checkout_date}"))`
-        db.query(query, (err, res) => {
+                select house_id from booking
+                where (checkin_date >= ? and checkin_date < ? )
+                or (checkout_date > ? and checkout_date < ? ))`
+        const params = [filter.checkin_date, filter.checkout_date, filter.checkin_date, filter.checkout_date]
+        db.query(query, params, (err, res) => {
             if (err) {
                 console.log(err);
             } else {
