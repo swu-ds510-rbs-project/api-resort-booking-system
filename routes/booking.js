118263cefbdc1d9e9ba4360cc22e00b6fe252016
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-22 01:54:19 +0700

    Add get booking path

--- routes/booking.js
+++ routes/booking.js
@@ -4,5 +4,6 @@ import BookingController from "../controllers/booking.js"
 const router = express.Router()
 
 router.post("/", BookingController.apiAddBooking)
+router.post("/:guestId", BookingController.apiGetBookingsByGuest)
 
 export default router
